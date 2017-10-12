98ba75d8056f8a7971bf7d8b21971be5db6f77a8
qinghe <areq22@aliyun.com>
2017-10-12 10:44:52 +0800

    add table hot_city

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171011065728) do
+ActiveRecord::Schema.define(version: 20171011145626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,6 +95,17 @@ ActiveRecord::Schema.define(version: 20171011065728) do
     t.datetime "updated_at", null: false
     t.integer  "user_id"
     t.index ["user_id"], name: "index_events_on_user_id", using: :btree
+  end
+
+  create_table "hot_cities", force: :cascade do |t|
+    t.string   "name"
+    t.string   "pingyi"
+    t.string   "acronyms"
+    t.string   "province"
+    t.string   "status"
+    t.string   "integer"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "images", force: :cascade do |t|
